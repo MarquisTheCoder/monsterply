c653e1111aeadd15ccdfd5b41c73acd412c68180
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-07 23:36:19 -0500

    auto save script Fri Apr  7 23:36:19 CDT 2023

--- monsterapply/app.py
+++ monsterapply/app.py
@@ -1,2 +1,10 @@
 
 from traversing.paths.recommended_jobs import RecommendedPaths
+from traversing.paths.home import HomePaths
+
+def main():
+    print(f'homepage -> home button = {HomePaths.home}')
+    print(f'recommended page -> home button = {RecommendedPaths.home}')
+
+if __name__ == '__main__':
+    main()
