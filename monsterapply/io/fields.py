30c8b55d9574417602d263c0d0ed74fd4d82d610
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-08 03:33:12 -0500

    auto save script Sat Apr  8 03:33:11 CDT 2023

--- monsterapply/io/fields.py
+++ monsterapply/io/fields.py
@@ -10,14 +10,23 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
 
-import random
+from time import sleep
+from random import choice
+from math import floor
 
-def _read(message: str):
-    pass
+def calculate_type_speed(wpm: int):
+    return floor(choice([300, 400, 400, 500,600]) * 10 / wpm) 
 
 def send(message: str, into: XPATH, driver: WebDriver) -> None:
+
     try:
         field: WebElement = wait(to_find=into, driver=driver)
+
+        for character in message:
+            field.send_keys(character)
+            calculate_type_speed(90)
+
+        field.send_keys(Keys.ENTER)
         
     except Exception:
         pass
