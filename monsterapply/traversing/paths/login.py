f54c7bcf17a7022453058b425190ecf373068e5f
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 21:04:32 -0500

    auto save script Sun Apr  9 21:04:32 CDT 2023

--- monsterapply/traversing/paths/login.py
+++ monsterapply/traversing/paths/login.py
@@ -1,7 +1,7 @@
 from .types import XPATH
 
 class Login:
-    google_login: XPATH = '//*[@id="__next"]/div[2]/div/div[3]/div/div/main/form/div[3]/div[3]/button'
+    google_login: XPATH = '/html/body/div[1]/div/div[2]/div/div[3]/div/div/main/form/div[3]/div[3]/button'
     facebook_login: XPATH = '//*[@id="__next"]/div[2]/div/div[3]/div/div/main/form/div[3]/div[2]/button'
     apple_login: XPATH = '//*[@id="__next"]/div[2]/div/div[3]/div/div/main/form/div[3]/div[1]/button'
     email: XPATH = '//*[@id="email"]'
