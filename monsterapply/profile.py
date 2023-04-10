2cfd9168f435504617f0ba7f3df5b76a40852d80
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 23:20:54 -0500

    auto save script Sun Apr  9 23:20:54 CDT 2023

--- monsterapply/profile.py
+++ monsterapply/profile.py
@@ -12,25 +12,12 @@ from traversing.paths.home import HomePaths
 import pickle
 import asyncio
 from time import sleep
-from itertools import count
-from concurrent.futures import ProcessPoolExecutor
 
 driver: WebDriver = new_driver()
-
-executor = ProcessPoolExecutor(2)
-loop = asyncio.new_event_loop()
-baa = loop.run_in_executor(executor, save_cookies)
-
-loop.run_forever()
 
 def start_driver():
     driver.get("https://google.com")
     wait(HomePaths.home, driver=driver, timeout=100000)
-
-
-def save_cookies():
-    for _ in count(0):
-        sleep(0.5)
-        pickle.dump(driver.get_cookies(), open("cookies/cookies.pkl", "wb"))
+    pickle.dump(driver.get_cookies(), open("cookies/cookies.pkl", "wb"))
 
 start_driver()
