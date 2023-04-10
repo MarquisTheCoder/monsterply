ac0ab06d0a55f28864eff345810b47d2540427b2
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-10 01:31:15 -0500

    auto save script Mon Apr 10 01:31:14 CDT 2023

--- monsterapply/mio/fields.py
+++ monsterapply/mio/fields.py
@@ -1,39 +1,48 @@
-
-
-import random
 from time import sleep
-from math import floor
-
-from .wait import *
-
-
+from random import uniform, normalvariate, randint, triangular, random
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 
 def _calculate_type_speed(wpm: int) -> None:
+    """Wait for a random amount of time to simulate human typing speed."""
     words_per_second = wpm / 60
-    delay = random.expovariate(words_per_second)  # use exponential distribution for natural typing rhythm
+    delay = normalvariate(1/words_per_second, 0.1)  # use normal distribution for typing rhythm
     sleep(delay)
 
-def send(message: str, into: WebElement, driver: WebDriver, wpm: int = 120) -> None:
+
+def _simulate_typing_error(into: WebElement, message: str, i: int) -> None:
+    """Simulate a typing error by backspacing and retyping the previous character."""
+    sleep(uniform(0.1, 0.3))
+    into.send_keys(Keys.BACKSPACE)
+    sleep(uniform(0.1, 0.3))
+    into.send_keys(message[i-1])
+
+
+def send(message: str, into: WebElement, driver: WebDriver, wpm: int = 60) -> None:
     """Type a message into an input field."""
     try:
-        for character in message:
+        for i in range(len(message)):
+            character = message[i]
             into.send_keys(character)
             _calculate_type_speed(wpm)
-            if random.random() < 0.1:
-                backspace_count = random.randint(1, 3)
-                backspace_delay = random.uniform(0.5, 1.5)
+            if random() < 0.01:  # simulate a typing error with a probability of 1%
+                _simulate_typing_error(into, message, i)
+            if random() < 0.02:  # add a pause with a probability of 2%
+                pause_length = triangular(0.1, 0.5, 0.3)  # use triangular distribution for pause length
+                sleep(pause_length)
+            if random() < 0.02:  # backspace with a probability of 2%
+                backspace_count = randint(1, 3)
+                backspace_delay = uniform(0.5, 1.5)
                 sleep(backspace_delay)
-                for i in range(backspace_count):
+                for j in range(backspace_count):
                     into.send_keys(Keys.BACKSPACE)
-                    sleep(random.uniform(0.1, 0.3))
-                for i in range(backspace_count):
-                    into.send_keys(message[-i-1])
+                    sleep(uniform(0.1, 0.3))
+                for j in range(backspace_count):
+                    into.send_keys(message[i-j-1])
                     _calculate_type_speed(wpm)
-        sleep(random.uniform(0.5, 1))
+        sleep(uniform(0.5, 1))  # add a pause at the end
         into.send_keys(Keys.RETURN)
 
     except Exception:
