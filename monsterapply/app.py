11362048b9fd155c3248a42ceffc4eab5c13b306
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 19:01:55 -0500

    auto save script Sun Apr  9 19:01:55 CDT 2023

--- monsterapply/app.py
+++ monsterapply/app.py
@@ -4,12 +4,13 @@ from traversing.paths.home import HomePaths
 from utils.useragents import get_user_agent
 from cli import file
 
+from crawling import Crawler
 
 def main() -> None:
     print(f'homepage -> home button = {HomePaths.home}')
     print(f'recommended page -> home button = {RecommendedPaths.home}')
     print(get_user_agent())
-    file()
+    # file()
 
     crawler: crawling.Crawler = crawling.Crawler()
     crawler.crawl()
