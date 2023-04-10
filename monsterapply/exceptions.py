f8bf82f86efae20567def9e714f3686fd116b5be
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 19:10:20 -0500

    auto save script Sun Apr  9 19:10:20 CDT 2023

--- monsterapply/exceptions.py
+++ monsterapply/exceptions.py
@@ -5,7 +5,9 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 def raises_not_found(action: Callable(...)) -> None:
-    try:
-        action()
-    except NoSuchElementException(Exception):
-        print(Exception.with_traceback)
+    def check_script(*args, **kwargs):
+        try:
+            action(*args, **kwargs)
+        except NoSuchElementException(Exception):
+            Exception.with_traceback()
+    return check_script
