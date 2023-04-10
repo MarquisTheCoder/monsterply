24f79c94019f704c052d9ebd2447d2bfabd78cf7
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 23:57:30 -0500

    auto save script Sun Apr  9 23:57:30 CDT 2023

--- monsterapply/mio/fields.py
+++ monsterapply/mio/fields.py
@@ -13,18 +13,18 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 
-def _calculate_type_speed(wpm: int):
-    return choice((uniform(70, 90), 
+def _calculate_type_speed(wpm: int) -> None:
+    sleep(choice((uniform(70, 90), 
                    uniform(90,100), 
-                   uniform(95,100))) / (wpm * uniform(3,4))
+                   uniform(95,100))) 
+                   / (wpm * uniform(3,4)))
 
 def send(message: str, into: WebElement, driver: WebDriver) -> None:
 
     try:
         for character in message:
             into.send_keys(character)
-            sleep(_calculate_type_speed(90))
-
+            _calculate_type_speed
         randomize_pause(1,2.3)
         into.send_keys(Keys.ENTER)
 
