eb6a343b2115a394ed47a0b808d8b5e6b5a457d1
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 19:20:24 -0500

    auto save script Sun Apr  9 19:20:24 CDT 2023

--- monsterapply/driver.py
+++ monsterapply/driver.py
@@ -9,15 +9,16 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 from utils.useragents import get_user_agent
 
-"""Editing chrome options to avoid detection"""
-chrome_options: Options = Options()
-chrome_options.add_experimental_option("detach", True)
-chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])  
-chrome_options.add_experimental_option("useAutomationExtension", False) 
-chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
-chrome_options.add_argument(f'user-agent={get_user_agent()}')
-
 def new_driver() -> WebDriver:
-    with uc2.Chrome(version_main=111, chrome_options=chrome_options) as chrome:
-        chrome.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
-        return chrome
+
+    """Editing chrome options to avoid detection"""
+    chrome_options: Options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])  
+    chrome_options.add_experimental_option("useAutomationExtension", False) 
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled") 
+    chrome_options.add_argument(f'user-agent={get_user_agent()}')
+
+    chrome: WebDriver = uc2.Chrome(version_main=111, chrome_options=chrome_options)
+    chrome.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
+    return chrome
