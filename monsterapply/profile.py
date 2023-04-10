3e307005125deab32c01116b0d3631e1a4b6eedd
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 22:50:06 -0500

    auto save script Sun Apr  9 22:50:06 CDT 2023

--- monsterapply/profile.py
+++ monsterapply/profile.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from driver import new_driver
-
+from mio.wait import wait
+from traversing.paths.home import HomePaths
 driver: WebDriver = new_driver()
 driver.get("https://google.com")
+wait(HomePaths.home, driver=driver, timeout=100000)
