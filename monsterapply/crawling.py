1c01e4ffc3dd6b8884b5d09cd7d0c9ab41c27311
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-09 22:39:13 -0500

    auto save script Sun Apr  9 22:39:13 CDT 2023

--- monsterapply/crawling.py
+++ monsterapply/crawling.py
@@ -54,8 +54,6 @@ class Crawler():
         self.goto_home()
         self.open_login_page()
         sleep(1000000)
-        # self.bypass_login('deshawn.m.williams01@gmail.com', 'Ciddate0!')
-        # self.search_job("Python Developer")
 
     def goto_home(self) -> None:
         self.driver.get(base_url)
