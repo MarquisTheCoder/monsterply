69cb9755c3dbb7a8671c57cd2bee9924a871992e
MarquistheCoder <deshawn.m.williams01@gmail.com>
2023-04-10 01:26:17 -0500

    auto save script Mon Apr 10 01:26:17 CDT 2023

--- monsterapply/mio/fields.py
+++ monsterapply/mio/fields.py
@@ -1,7 +1,6 @@
 
 
-
-from random import choice, uniform
+import random
 from time import sleep
 from math import floor
 
@@ -14,20 +13,28 @@ from selenium.webdriver.remote.webelement import WebElement
 
 
 def _calculate_type_speed(wpm: int) -> None:
-    sleep(choice((uniform(70, 90), 
-                   uniform(90,100), 
-                   uniform(95,100))) 
-                   / (wpm * uniform(3,4)))
+    words_per_second = wpm / 60
+    delay = random.expovariate(words_per_second)  # use exponential distribution for natural typing rhythm
+    sleep(delay)
 
-def send(message: str, into: WebElement, driver: WebDriver) -> None:
-
+def send(message: str, into: WebElement, driver: WebDriver, wpm: int = 120) -> None:
+    """Type a message into an input field."""
     try:
         for character in message:
             into.send_keys(character)
-            _calculate_type_speed
-        randomize_pause(1,2.3)
-        into.send_keys(Keys.ENTER)
+            _calculate_type_speed(wpm)
+            if random.random() < 0.1:
+                backspace_count = random.randint(1, 3)
+                backspace_delay = random.uniform(0.5, 1.5)
+                sleep(backspace_delay)
+                for i in range(backspace_count):
+                    into.send_keys(Keys.BACKSPACE)
+                    sleep(random.uniform(0.1, 0.3))
+                for i in range(backspace_count):
+                    into.send_keys(message[-i-1])
+                    _calculate_type_speed(wpm)
+        sleep(random.uniform(0.5, 1))
+        into.send_keys(Keys.RETURN)
 
     except Exception:
         pass
-    
